28d5491a07cf2ac584b241de17962b0f9742d47c
Paul Dale <ldpercy@gmail.com>
2026-01-15 16:09:15 +1100

    Add maths random int funcs

--- module/Maths.js
+++ module/Maths.js
@@ -88,5 +88,27 @@ export function degrees180(degrees) {
 
 
 
+/** getRandomInt
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+ * @param {number} min
+ * @param {number} max
+ * @returns {number}
+ */
+export function getRandomInt(min, max) {
+	const minCeiled = Math.ceil(min);
+	const maxFloored = Math.floor(max);
+	return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
+}
 
+/** getRandomIntInclusive
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+ * @param {number} min
+ * @param {number} max
+ * @returns {number}
+ */
+export function getRandomIntInclusive(min, max) {
+	const minCeiled = Math.ceil(min);
+	const maxFloored = Math.floor(max);
+	return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled); // The maximum is inclusive and the minimum is inclusive
+}
 
