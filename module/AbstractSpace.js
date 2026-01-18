164742834a583e105e9fb1043d8d7939292c52a2
Paul Dale <ldpercy@gmail.com>
2026-01-18 14:50:48 +1100

    New task: space parameters

--- module/AbstractSpace.js
+++ module/AbstractSpace.js
@@ -3,15 +3,25 @@
 //
 
 
+
+
+
+
 // not sure if should be base class or interface...
 
 export class Space {
 
 	/** @type string */
 	#name;
+	#setting;
 
-	constructor(name) {
+
+	constructor(name, setting) {
 		this.#name = name;
+		this.#setting = setting;
 	}
 
-}
+	get name() { return this.#name; }
+	get setting() { return this.#setting; }
+
+}/* Space */
